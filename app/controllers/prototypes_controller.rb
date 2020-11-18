1dcbd37687d18b20fd2334385929b107f7f5ff71
taro-sakakibara <theband0914@gmail.com>
2020-11-18 22:30:05 +0900

    修正３

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -22,7 +22,7 @@ class PrototypesController < ApplicationController
   def show
     @prototype = Prototype.find(params[:id])
     @comment = Comment.new
-    @comments = @prototype.comments.includes(:user)
+    @comments = @prototype.comment.includes(:user)
   end
 
   def edit
--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -4,7 +4,7 @@ class Prototype < ApplicationRecord
   validates :cath_copy, presence: true
   validates :concept, presence: true
   
-  has_one_attached :image
+  has_one_attached :image, dependent: :destroy
   has_many :comment, dependent: :destroy
 
   belongs_to :user
