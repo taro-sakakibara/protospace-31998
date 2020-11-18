df85ba557760d9c55d3ffa6103bdaa8622850b93
taro-sakakibara <theband0914@gmail.com>
2020-11-18 22:19:15 +0900

    修正３

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -5,6 +5,7 @@ class Prototype < ApplicationRecord
   validates :concept, presence: true
   
   has_one_attached :image
-  has_many :comments
+  has_many :comment, dependent: :destroy
+
   belongs_to :user
 end
