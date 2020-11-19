f4317d5206da6bd28a752d1002388483113b5933
taro-sakakibara <theband0914@gmail.com>
2020-11-19 11:16:28 +0900

    修正４

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show,]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @prototypes = Prototype.all
@@ -12,7 +12,8 @@ class PrototypesController < ApplicationController
   end
 
   def create
-    if Prototype.create(prototype_params)
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
      redirect_to root_path
     else
      render new_prototype_path
@@ -29,15 +30,14 @@ class PrototypesController < ApplicationController
     unless user_signed_in?
       redirect_to action: :index
     end
-    @prototype = Prototype.find(params[:id])
+    # @prototype = Prototype.find(params[:id])
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    prototype.update(prototype_params)
+    @prototype = Prototype.find(params[:id])
 
-    if prototype.update(prototype_params)
-      redirect_to prototype_path(prototype.id)
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype.id)
     else
       #  どうしてedit_prototype_pathはだめなのだろう。。。
       render :edit
